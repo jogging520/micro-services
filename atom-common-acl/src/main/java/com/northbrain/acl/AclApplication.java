631266093f003715c68d06f82b4ade908d62a257
446734796@qq.com <wuqiang502>
2018-04-04 14:30:48 +0800

    测试提交

---
+++ atom-common-acl/src/main/java/com/northbrain/acl/AclApplication.java
@@ -0,0 +1,12 @@
+package com.northbrain.acl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AclApplication {
+//jk
+	public static void main(String[] args) {
+		SpringApplication.run(AclApplication.class, args);
+	}
+}
