6b301e725c3c13eec4d8e07351480cbd4f22c58e
446734796@qq.com <wuqiang502>
2018-04-08 11:58:32 +0800

    用了fluent

--- atom-common-session/src/main/java/com/northbrain/session/Session.java
+++ atom-common-session/src/main/java/com/northbrain/session/Session.java
@@ -5,10 +5,14 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Builder;
+import lombok.experimental.Accessors;
 
 import java.util.Date;
 
 @Data
+@Accessors(fluent=true, chain=true)
+@Builder
 @Document
 @NoArgsConstructor
 public class Session {
--- atom-common-session/src/main/java/com/northbrain/session/SessionService.java
+++ atom-common-session/src/main/java/com/northbrain/session/SessionService.java
@@ -22,38 +22,35 @@ public class SessionService {
 
     //如果不存在，那么创建一条，否则返回已经登录。
     public Mono<Session> saveSession(String channelType, String userId, String roleId, String organizationId) {
-
-        Session newSession = new Session();
-        newSession.setChannelType(channelType);
-        newSession.setUserId(userId);
-        newSession.setRoleId(roleId);
-        newSession.setOrganizationId(organizationId);
-        newSession.setCreateTime(new Date());
-        newSession.setLoginTime(new Date());
-        newSession.setTimestamp(null);
-        newSession.setStatus("ACTIVE");
-        newSession.setLifeTime(this.jsonWebToken.getLifeTime());
-
         return this.sessionRepository
                 .findByTypeAndUserId(channelType, userId)
                 .switchIfEmpty(
-                             sessionRepository.save(newSession)
+                             sessionRepository.save(Session
+                                     .builder()
+                                     .channelType(channelType)
+                             .userId(userId)
+                             .roleId(roleId)
+                             .organizationId(organizationId)
+                             .createTime(new Date())
+                             .loginTime(new Date())
+                             .timestamp(null)
+                             .status("ACTIVE")
+                             .lifeTime(this.jsonWebToken.getLifeTime())
+                             .build())
                         )
-                .flatMap(session -> {
-                    Session newSession1 = new Session();
-                    newSession1.setSessionId(session.getSessionId());
-                    newSession1.setChannelType(channelType);
-                    newSession1.setUserId(userId);
-                    newSession1.setRoleId(roleId);
-                    newSession1.setOrganizationId(organizationId);
-                    newSession1.setCreateTime(session.getCreateTime());
-                    newSession1.setLoginTime(new Date());
-                    newSession1.setTimestamp(null);
-                    newSession1.setStatus("ACTIVE");
-                    newSession1.setLifeTime(this.jsonWebToken.getLifeTime());
-
-                    return sessionRepository.save(newSession1);
-                })
+                .flatMap(session -> sessionRepository.save(Session.builder()
+                    .sessionId(session.sessionId())
+                    .channelType(channelType)
+                    .userId(userId)
+                    .roleId(roleId)
+                    .organizationId(organizationId)
+                    .createTime(session.createTime())
+                    .loginTime(new Date())
+                    .timestamp(new Date())
+                    .status("ACTIVE")
+                    .lifeTime(this.jsonWebToken.getLifeTime())
+                    .build())
+                )
                 ;
     }
 }
